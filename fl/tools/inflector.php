4cfbe652e60bac2be421eedb8871a9304ad3f293
kronn <kronn@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2008-11-06 04:28:03 +0000

    fl_inflector::is_singular($word) hinzugefügt
    git-svn-id: http://svn.assembla.com/svn/federleicht/trunk@127 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- fl/tools/inflector.php
+++ fl/tools/inflector.php
@@ -11,10 +11,12 @@ class fl_inflector {
 	/**
 	 * Speicher für Ersetzungsregeln
 	 */
-	var $plural = array();
-	var $singular = array();
-	var $uncountable = array();
-	var $irregular = array();
+	protected $plural = array();
+	protected $singular = array();
+	protected $uncountable = array();
+	protected $irregular = array();
+
+	protected $msg = array();
 
 	/**
 	 * Konstruktor
@@ -24,7 +26,6 @@ class fl_inflector {
 		case 'de':
 			$this->plural = array(
 				'/^([a-z]*)(ung|heit|schaft|ion)$/i'=>'\1\2en',
-				'/^([a-z]*)(probe)$/i'=>'\1\2n',
 				'/^([a-z]*)([ei])(ld)$/i'=>'\1\2\3er',
 				'/^([a-z]*)([auo])(ld|ch|nd)$/i'=>'\1\2e\3er',
 				'/^([a-z]*[^aoieukgh])$/i' =>'\1e',
@@ -32,7 +33,6 @@ class fl_inflector {
 
 			$this->singular = array(
 				'/^([a-z]*)(ung|heit|schaft|ion)en$/i'=>'\1\2',
-				'/^([a-z]*)(probe)n$/i'=>'\1\2',
 				'/^([a-z]*)([ei])(ld)er$/i'=>'\1\2\3',
 				'/^([a-z]*)([auo])e(ld|ch|nd)er$/i'=>'\1\2\3',
 				'/^([a-z]*)e/i' => '\1',
@@ -52,6 +52,10 @@ class fl_inflector {
 			$this->irregular = array(
 				'account' => 'accounts',
 				'land' => 'laender',
+				'beat' => 'beats',
+				'album' => 'alben',
+				'genre' => 'genres',
+				'hoerprobe' => 'hoerproben'
 			);
 			break;
 
@@ -146,9 +150,9 @@ class fl_inflector {
 			} 
 		} 
 
-		foreach ($irregular as $_plural=> $_singular){ 
-			if (preg_match('/('.$_plural.')$/i', $word, $arr)) { 
-				return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word); 
+		foreach ($irregular as $_singular=> $_plural){
+			if (preg_match('/('.$_singular.')$/i', $word, $arr)) { 
+				return preg_replace('/('.$_singular.')$/i', substr($arr[0],0,1).substr($_plural,1), $word); 
 			} 
 		} 
 
@@ -173,15 +177,16 @@ class fl_inflector {
 		$irregular = $this->irregular;
 
 		$lowercased_word = strtolower($word);
+
 		foreach ($uncountable as $_uncountable){
 			if(substr($lowercased_word,(-1*strlen($_uncountable))) == $_uncountable){
 				return $word;
 			}
 		}
 
-		foreach ($irregular as $_plural=> $_singular){
-			if (preg_match('/('.$_singular.')$/i', $word, $arr)) {
-				return preg_replace('/('.$_singular.')$/i', substr($arr[0],0,1).substr($_plural,1), $word);
+		foreach ($irregular as $_singular=> $_plural){
+			if (preg_match('/('.$_plural.')$/i', $word, $arr)) {
+				return preg_replace('/('.$_plural.')$/i', substr($arr[0],0,1).substr($_singular,1), $word);
 			}
 		}
 
@@ -192,5 +197,75 @@ class fl_inflector {
 		}
 
 		return $word;
+	}
+
+	function is_singular($word) {
+		$has_singular_form = true;
+		$has_plural_form = false;
+
+		if ( in_array( strtolower($word), $this->uncountable ) ) {
+			$this->msg[] = $word . ' is uncountable';
+			return $has_singular_form;
+		} else {
+			foreach( $this->irregular as $singular => $plural ) {
+				if ( stripos($singular, $word) !== false ) {
+					$this->msg[] = $word . ' is irregular and singular';
+					return $has_singular_form;
+				} elseif ( stripos($plural, $word) !== false ) {
+					$this->msg[] = $word . ' is irregular and plural';
+					return false;
+				}
+			}
+
+			$can_be_singular = false;
+			$can_be_plural = false;
+
+			foreach( $this->singular as $rule => $replacement ) {
+				if ( preg_match($rule, $word) ) {
+					$this->msg[] = $word . ' can be converted to singular (rule: '.$rule.' => '.$this->singular($word).')';
+					$can_be_singular = true;
+					break;
+				} 
+			}
+
+			foreach( $this->plural as $rule => $replacement ) {
+				if ( preg_match($rule, $word) ) {
+					$this->msg[] = $word . ' can be converted to plural (rule: '.$rule.' => '.$this->plural($word).')';
+					$can_be_plural = true;
+					break;
+				} 
+			}
+
+			if ( $can_be_plural and !$can_be_singular ) {
+				return $has_singular_form;
+			} elseif ( $can_be_singular and !$can_be_plural ) {
+				return $has_plural_form;
+			} elseif ( $can_be_singular and $can_be_plural ) {
+				$might_be_plural = $this->plural($this->singular($word)) === $word;
+				$might_be_singular = $this->singular($this->plural($word)) === $word;
+
+				if ( $might_be_plural and !$might_be_singular ) {
+					return $has_plural_form;
+				} elseif ( $might_be_singular and !$might_be_plural) {
+					return $has_singular_form;
+				} elseif ( $might_be_plural and $might_be_singular ) {
+					try {
+						if ( $this->is_singular($this->singular($word)) ) {
+							return $has_plural_form;
+						} elseif ( ! $this->is_singular($this->plural($word)) ) {
+							return $has_singular_form;
+						}
+					} catch ( RuntimeException $e ) {
+						throw new LogicException('Inflector muss überarbeitet werden, um bessere Regeln für "'.$word.'" zu haben');
+					}
+				}
+			} else {
+				throw new RuntimeException('Es kann nicht herausgefunden werden, ob "'.$word.'" Singular oder Plural ist. ' . PHP_EOL . $this);
+			}
+		}
+	}
+
+	public function __toString() {
+		return implode(', ', $this->msg);
 	}
 }
