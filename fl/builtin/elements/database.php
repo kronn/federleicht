026e2a66fcd02c017f106c644da5b781f1b632b5
kronn <kronn@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2009-04-21 23:02:25 +0000

    Fehlermeldungen produzieren keine Fehler wenn keine Datenbank verwendet wird.
    git-svn-id: http://svn.assembla.com/svn/federleicht/trunk@212 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- fl/builtin/elements/database.php
+++ fl/builtin/elements/database.php
@@ -1,7 +1,10 @@
-<?php $has_db_query = file_exists(FL_ABSPATH.'public/php/db_query.php'); // @todo anclickbare SQL-Queries erzeugen ?>
+<?php if ( $this->get('db') instanceof fl_data_access_database ) { ?>
+
+	<?php $has_db_query = file_exists(FL_ABSPATH.'public/php/db_query.php'); // @todo anclickbare SQL-Queries erzeugen ?>
 
 		<h3>Datenbankabfragen</h3>
 		<pre>
-<?php var_dump($this->get('db')->export_query_log()); ?>
+	<?php var_dump($this->get('db')->export_query_log()); ?>
 		</pre>
 
+<?php } ?>
